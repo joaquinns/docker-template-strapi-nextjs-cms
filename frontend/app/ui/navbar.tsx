abd442173d8833c95c7d565600a6e9066eba1755
joaquinns <joaquindevv96@gmail.com>
2025-01-06 23:35:35 -0400

    update Navbar styles for improved visibility and backdrop effects

--- frontend/app/ui/navbar.tsx
+++ frontend/app/ui/navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg shadow-sm shadow-white/40 h-16">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-[#d494b5]/90  backdrop-filter backdrop-blur-lg shadow-sm shadow-white/40 h-16">
       <nav className="relative max-w-6xl mx-auto px-4 lg:px-0 h-full">
         <div className="flex justify-between items-center h-full lg:px-4">
           <div>logo</div>
@@ -35,7 +35,7 @@ export const Navbar = () => {
           </button>
 
           <ul
-            className={`fixed top-16 left-0 right-0 p-4 flex-col w-full h-screen font-bold text-2xl lg:flex lg:gap-4 lg:justify-center lg:items-center lg:font-semibold lg:text-base lg:flex-row lg:relative lg:w-auto lg:h-full lg:top-0 lg:p-0 lg:bg-transparent lg:bg-opacity-100 lg:backdrop-filter-none lg:backdrop-blur-none bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg lg:translate-y-0 transition-transform duration-500 ease-in-out ${
+            className={`fixed top-16 left-0 right-0 p-4 flex-col w-full h-screen font-bold text-2xl lg:flex lg:gap-4 lg:justify-center lg:items-center lg:font-semibold lg:text-base lg:flex-row lg:relative lg:w-auto lg:h-full lg:top-0 lg:p-0 lg:bg-transparent lg:bg-opacity-100 lg:backdrop-filter-none lg:backdrop-blur-none bg-white bg-opacity-20 backdrop-filter backdrop-blur-xl lg:translate-y-0 transition-transform duration-500 ease-in-out ${
               isMenuOpen
                 ? "transform translate-y-0"
                 : "transform -translate-y-[calc(100%+64px)]"
