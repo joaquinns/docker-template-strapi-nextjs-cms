a4d0fe082c9f4e5d523ec1450756573eeed6042e
joaquinns <joaquindevv96@gmail.com>
2025-01-07 19:40:35 -0400

    enhance Card component with expandable description and add CartAddIcon for improved cart interaction

--- frontend/app/ui/components/card.tsx
+++ frontend/app/ui/components/card.tsx
@@ -1,58 +1,66 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { CartAddIcon } from "../icons/cart-add";
 
 interface CardProps {
   title?: string;
+  description?: string;
 }
 
-export const Card = ({ title = "Titulo de ejemplo" }: CardProps) => {
-  // const [isHovered, setIsHovered] = useState(false);
-  /*   const [isExpanded, setIsExpanded] = useState(false);
-  const [showExpandButton, setShowExpandButton] = useState(false);
-  const descriptionRef = useRef<HTMLParagraphElement>(null); */
-
-  // Detecta si el contenido excede 3 líneas
-  /*   useEffect(() => {
-    const element = descriptionRef.current;
-    if (element) {
-      const lineHeight = parseFloat(getComputedStyle(element).lineHeight);
-      const maxHeight = lineHeight * 3; // Altura para 3 líneas
-      if (element.scrollHeight > maxHeight) {
-        setShowExpandButton(true); // Mostrar "Ver más" si hay más de 3 líneas
-      }
-    }
-  }, []); */
+export const Card = ({
+  title = "Titulo de ejemplo",
+  description = "Esto es el ejemplo de una descripcion, puneta lambre bicho esto funciona? cabron conte cabron habla claro, brrrrrrr. Menos mal ya terminamos esa mierda de juego",
+}: CardProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <article className="relative flex flex-col justify-center items-center text-slate-900 transition-all ease-in-out duration-300 max-w-[400px] mx-auto overflow-hidden h-full">
+    <article className="relative flex flex-col justify-center items-center text-slate-900 transition-all ease-in-out duration-300 max-w-[400px] mx-auto h-full">
+      <Image
+        className="rounded"
+        height={260}
+        width={400}
+        alt="card image"
+        src="/heroflowers.jpg"
+        blurDataURL="/heroflowers.jpg"
+        loading="lazy"
+        style={{ height: "260px", objectFit: "cover" }}
+      />
       <Link href="#">
-        <Image
-          className="rounded"
-          height={260}
-          width={400}
-          alt="card image"
-          quality={80}
-          loading="lazy"
-          src="/heroflowers.jpg"
-          style={{ height: "260px", objectFit: "cover" }}
-        />
-        <div className="flex flex-col">
-          <h3 className="p-2 text-xl font-bold">{title}</h3>
-          <div className="p-2 font-semibold">
-            <span className="text-lg">$10</span>
-          </div>
+        <h3 className="p-2 text-xl font-bold hover:underline">{title}</h3>
+        <div className="px-2 font-semibold">
+          <span className="text-lg">$10</span>
+        </div>
+        <div className="p-2 flex-1 relative">
+          <p
+            className={`text-base ${
+              isExpanded ? "line-clamp-none" : "line-clamp-2"
+            }`}
+          >
+            {description}
+          </p>
           <button
             onClick={(e) => {
               e.preventDefault();
-              console.log("added :D");
+              setIsExpanded(!isExpanded);
             }}
-            className="py-2 px-4 bg-slate-800 transition-all ease-in-out duration-200 rounded"
+            className="text-sm text-primary hover:underline mt-2"
           >
-            add to cart :D
+            {isExpanded ? "Ver menos" : "Ver más"}
           </button>
         </div>
       </Link>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("added :D");
+        }}
+        className="py-2 px-4 bg-[#d79ebc] hover:bg-[#ca79a4] transition-all ease-in-out duration-200 rounded ml-auto font-semibold flex gap-2 justify-center items-center"
+      >
+        <CartAddIcon />
+        Agregar
+      </button>
     </article>
   );
 };
