21f97fb87a825edd045e4d42426a1eb35dc81df0
joaquinns <joaquindevv96@gmail.com>
2025-01-09 16:53:30 -0400

    replace DamasHero with SubHero component and add Hombres page for improved layout and user experience

--- frontend/app/hombres/page.tsx
+++ frontend/app/hombres/page.tsx
@@ -2,16 +2,16 @@ import { Card } from "../ui/components/card";
 import { SearchBar } from "../ui/components/search-bar";
 import { SubHero } from "../ui/components/Subhero";
 
-export default function Damas() {
+export default function Hombres() {
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-6xl py-8 px-4 xl:px-0 text-slate-900">
         <SearchBar />
       </div>
       <SubHero
+        altImage="Damas hero image alt"
         imgURL="/whisky.jpg"
-        altImage="Hero image from caballeros page"
-        title="Regalos para Hombres"
+        title="Regalos para Caballeros"
       />
       <section className="max-w-6xl px-4 xl:px-0 py-8 mx-auto">
         <div
