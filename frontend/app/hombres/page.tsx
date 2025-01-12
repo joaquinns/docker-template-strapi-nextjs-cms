c9b16c639606114743603710b93b2f9834697a3b
joaquinns <joaquindevv96@gmail.com>
2025-01-12 19:10:52 -0400

    refactor Damas and Hombres pages to use GridSearchResults component for improved product display and search functionality

--- frontend/app/hombres/page.tsx
+++ frontend/app/hombres/page.tsx
@@ -1,8 +1,15 @@
-import { Card } from "../ui/components/card";
+import { GridSearchResults } from "../ui/components/grid-results";
 import { SearchBar } from "../ui/components/search-bar";
 import { SubHero } from "../ui/components/Subhero";
 
-export default function Hombres() {
+export default async function Hombres({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const params = await searchParams;
+  const { q } = params;
+
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-6xl py-8 px-4 xl:px-0 text-slate-900">
@@ -14,20 +21,7 @@ export default function Hombres() {
         title="Regalos para Caballeros"
       />
       <section className="max-w-6xl px-4 xl:px-0 py-8 mx-auto">
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-            gap: "1rem",
-          }}
-        >
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <GridSearchResults query={q} man={true} />
       </section>
     </div>
   );
