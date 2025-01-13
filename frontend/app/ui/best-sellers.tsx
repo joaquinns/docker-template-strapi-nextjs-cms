08d3f51aa82ab730b22df6a667a7c3852bc8ce7b
joaquinns <joaquindevv96@gmail.com>
2025-01-13 01:13:37 -0400

    update BestSellers component to use SectionHeader and map products for dynamic rendering

--- frontend/app/ui/best-sellers.tsx
+++ frontend/app/ui/best-sellers.tsx
@@ -1,11 +1,39 @@
 import { Card } from "./components/card";
+import SectionHeader from "./components/section-headers";
+
+const PRODUCTS = [
+  {
+    id: "1",
+    name: "Ramo de Rosas Blancas",
+    price: 29.99,
+    description:
+      "Esto es un ejemplo de descripcion, esto es una descripcion de ejemplo",
+    quantity: 1,
+  },
+  {
+    id: "2",
+    name: "Arreglo de Lirios",
+    description:
+      "Esto es un ejemplo de descripcion, esto es una descripcion de ejemplo",
+    price: 34.99,
+    quantity: 1,
+  },
+  {
+    id: "3",
+    name: "Caja de Chocolates Gourmet",
+    description:
+      "Esto es un ejemplo de descripcion, esto es una descripcion de ejemplo",
+    price: 24.99,
+    quantity: 1,
+  },
+];
 
 export const BestSellers = () => {
   return (
     <section className="relative min-h-screen mx-auto max-w-6xl px-4 xl:px-0">
-      <h1 className="font-semibold text-slate-900 text-4xl my-12 text-center">
-        Destacados
-      </h1>
+      <div className="my-16">
+        <SectionHeader title="Destacados" align="center" />
+      </div>
       <div
         style={{
           display: "grid",
@@ -13,9 +41,15 @@ export const BestSellers = () => {
           gap: "1rem",
         }}
       >
-        <Card />
-        <Card />
-        <Card />
+        {PRODUCTS.map((product) => (
+          <Card
+            key={product.id}
+            product={product}
+            id={product.id}
+            description={product.description}
+            title={product.name}
+          />
+        ))}
       </div>
     </section>
   );
